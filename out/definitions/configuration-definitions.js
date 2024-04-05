52d889f1fa5cf8194674010e8537b48104454e29
Chaxwell <>
2024-04-05 23:04:32 +0200

    chore: do not include output js files in repo

--- out/definitions/configuration-definitions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EXTENSION_NAME = void 0;
-exports.EXTENSION_NAME = 'twigLinkResolver';
-//# sourceMappingURL=configuration-definitions.js.map
